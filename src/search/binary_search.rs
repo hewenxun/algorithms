792d9d1a65ac99ca0133941cef0dcbeb6b582da0
hewenxun <hewenxun@yeah.net>
2024-10-29 21:05:45 +0800

    binary search

--- src/search/binary_search.rs
+++ src/search/binary_search.rs
@@ -4,7 +4,7 @@ pub fn binary_search<T: Ord>(items: &[T], target: &T) -> Option<usize> {
     let (mut l, mut r) = (0, items.len());
     while l < r {
         let m = l + (r - l) / 2;
-        match items[m].cmp(target) {
+        match (&items[m]).cmp(target) {
             Ordering::Less => { l = m + 1 }
             Ordering::Greater => { r = m }
             Ordering::Equal => { return Some(m) }
