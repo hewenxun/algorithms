6f3a83664c32f310fac8b9f23f49bd7ea886ac86
hewenxun <hewenxun@yeah.net>
2024-10-29 20:10:36 +0800

    binary search

---
+++ src/search/binary_search.rs
@@ -0,0 +1,14 @@
+use std::cmp::Ordering;
+
+pub fn binary_search<T: Ord>(items: &[T], target: &T) -> Option<usize> {
+    let (mut l, mut r) = (0, items.len());
+    while l < r {
+        let m = l + (r - l) / 2;
+        match items[m].cmp(target) {
+            Ordering::Less => { l = m + 1 }
+            Ordering::Greater => { r = m }
+            Ordering::Equal => { return Some(m) }
+        }
+    }
+    None
+}
--- src/search/mod.rs
+++ src/search/mod.rs
@@ -1,2 +1,5 @@
 mod linear_search;
 pub use linear_search::*;
+
+mod binary_search;
+pub use binary_search::*;
